0e15406f8c5f899d7e8aee30f2510e233c96ffc3
TheAzne <63120629+TheAzne@users.noreply.github.com>
2022-01-22 11:16:14 +0100

    Testing

--- kino/test/pages.spec.js
+++ kino/test/pages.spec.js
@@ -3,8 +3,30 @@ import request from 'supertest';
 import app from '../src/app.js';
 
 
-test('Showing us movie list', async () =>{
+test('Showing us list of movies', async () =>{
     const response = await request(app)
-    .get('/')
+    .get('/movies')
     .expect(200);
+    expect(response.text.includes('The Dark Knight')).toBeTruthy();
+
+}) 
+
+
+test('Details of movies', async () =>{
+    const response = await request(app)
+    .get('/movies/4')
+    .expect(200);
+
+    expect(response.text.includes('The Dark Knight')).toBeTruthy();
+   
+}) 
+
+
+
+test('Details of movies', async () =>{
+    const response = await request(app)
+    .get('/movies/1')
+    .expect(200);
+
+    expect(response.text.includes('Shawshank')).toBeTruthy();
 }) 
